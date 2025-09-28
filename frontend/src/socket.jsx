7370b92689f6c933c9c3e595bffdb69ef744513e
mangesh <86924066+mangesh04@users.noreply.github.com>
2025-09-28 17:19:13 +0530

    Update socket.jsx

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-// export const socket = io("http://192.168.230.73:3001");
-export const socket = io("http://localhost:3001");
+
+export const socket = io("https://sevens-card-game-production.up.railway.app");
